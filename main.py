84322ec9fc45592c39d66ac25aa298381b2cf474
karino <karino@localhost>
2011-09-07 16:50:11 +0900

    Anyway, working with real data.

--- main.py
+++ main.py
@@ -16,28 +16,142 @@
 #
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+
+class NumericCell(db.Model):
+   table = db.StringProperty()
+   row = db.IntegerProperty()
+   col = db.IntegerProperty()
+   val = db.FloatProperty()
+
+
+class Title(db.Model):
+   table = db.StringProperty()
+   col = db.IntegerProperty()
+   title = db.StringProperty()
+
+class TableDescription(db.Model):
+   name = db.StringProperty()
+   description = db.TextProperty()
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+	html = """<html>
+<head><title>Web Tobin Server, upload csv</title></head>
+<body>
+<h1> Upload CSV</h1>
+<form id="upload_form" action="/upload" enctype="multipart/form-data" method="post">
+  CSV File: <input type="file" name="file" id="file" /> <br>
+  Table Name: <input type="text" name="tableName" /> <br>
+  Description:<br> <textarea cols="80" rows="30" name="description" ></textarea> <br>
+  <input type="submit" value="Upload" />
+</form>
+</body>
+</html>"""
+        self.response.out.write(html)
+
+import urllib2
 
 class TableHandler(webapp.RequestHandler):
     def get(self, tableName):
 	callback = self.request.get('callback')
-	body = """{"titles": ["date", "GDP", "consumption"],
- "types": ["integer", "numeric", "numeric"],
- "data": [[1980, 312712.7, 174382.7],
-[1981, 321490.5, 177074.9]]
-}""" 
+        body = []
+        body.append('{"titles": [')
+	
+	#print OK
+	#tableName = urllib2.unquote(tableName)
+	
+        #print OK, set OK but no hit
+        #tableName = urllib2.unquote(tableName)
+        #tableName = tableName.decode('utf-8')
+
+	#self.response.out.write('table=' + str(type(tableName)))
+        #return
+        
+        #tableName = tableName.encode('utf-8')
+        #tableName = tableName.decode('utf-8')
+	#self.response.out.write('table=' + unicode(tableName, 'utf-8'))
+        #tableName = unicode(tableName, 'utf-8')
+	#self.response.out.write('table=' + tableName)
+	#return 
+	tableName = urllib2.unquote(tableName)
+	titles = Title.all().filter('table = ', tableName).order('col').fetch(100)
+        first = True
+        titleLen = len(titles)
+        for title in titles:
+           if first:
+               first = False
+           else:
+              body.append(",")
+           body.append('"')
+           body.append(title.title)
+           body.append('"')
+        body.append('],"types": [')
+        first = True
+        for t in titles:
+           if first:
+               first = False
+           else:
+              body.append(',')
+           body.append('"numeric"')
+        body.append('],"data": [[' )
+        # TODO: num
+        cells = NumericCell.all().filter('table = ', tableName).order('row').order('col').fetch(10*titleLen)
+        cur = 0
+        first = True
+        for cell in cells:
+           if(cur != cell.row):
+              body.append("],[")
+              first = True
+              cur = cell.row
+           if first:
+              first = False
+           else:
+              body.append(",")
+           body.append(str(cell.val))
+        body.append("]]}")
+        body = "".join(body)
 	if callback:
 		body = ('%s(%s);' % (callback, body))
 	self.response.headers['Content-Type']='text/javascript'
 	self.response.headers['Content-Length'] = len(body)
 	self.response.out.write(body)
 
+import csv
+import StringIO
+
+class UploadHandler(webapp.RequestHandler): 
+   def post(self): 
+     fileData = self.request.get("file")
+     tableName = self.request.get("tableName").encode('utf-8')
+     # print OK, that is, not Unicode... self.response.out.write("tableName " + tableName)
+     if not fileData or not tableName:
+         return self.redirect('/')
+     description = self.request.get("description")
+     
+     putCand = []
+     table = TableDescription(name=tableName, description=description)
+     putCand.append(table)
+     
+     stringReader = csv.reader(StringIO.StringIO(fileData))
+     titles = stringReader.next()
+     # self.response.out.write("title! " + str(len(titles))  +"\n")
+     for i, title in enumerate(titles):
+        #self.response.out.write(title + ",") 
+        t = Title(table=tableName, col=i, title=title)
+        putCand.append(t)
+     # self.response.out.write("\n")
+     stringReader.next()
+     for nrow, row in enumerate(stringReader): 
+	for ncol, col in enumerate(row):
+           cell = NumericCell(table=tableName, row=nrow, col=ncol, val=float(col))
+           putCand.append(cell)
+     db.put(putCand)
+
 def main():
     application = webapp.WSGIApplication([('/', MainHandler),
+					  ('/upload', UploadHandler),
 					   ('/t/([^/]*)/json', TableHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
